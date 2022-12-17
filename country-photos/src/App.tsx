61b3885c97df56e1cf1e4b1c26d263a0c6b50d11
Aiyoy <ronkaarizona@gmail.com>
2022-12-17 16:28:59 +0300

    init: start project

---
+++ country-photos/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import Header from './Header/Header';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ country-photos/src/Header/Header.tsx
@@ -0,0 +1,10 @@
+import './header.css'
+
+function Header() {
+  return <header className='header'>
+    <div className='burger'></div>
+    <div className='header-title'>Country photos</div>
+  </header>
+}
+
+export default Header;
